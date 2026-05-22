3c14d19951635e6ee6765b5c33bad4e86bd8c9f9
Logan Ashamallah <loganash500@gmail.com>
2026-05-21 21:40:52 -0700

    Remove Analytics calendar helper text

--- components/analytics/AnalyticsCalendar.tsx
+++ components/analytics/AnalyticsCalendar.tsx
@@ -159,10 +159,6 @@ export function AnalyticsCalendar({
           </View>
         ))}
       </View>
-
-      <Text className="mt-3 text-xs text-gray-500">
-        Tap a day to view your log history for that date.
-      </Text>
     </View>
   );
 }
